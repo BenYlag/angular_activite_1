1fe0a7b4a84590461bc304059b6aba36d4bc4288
BenY CIRDOC <b.lagache@cirdoc.fr>
2018-10-19 14:02:33 +0200

    Pour correction...

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'activite1-angular';
+    posts = [
+        {
+            title: 'Mon premier Post',
+            content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce venenatis vulputate venenatis. Vivamus a sagittis arcu.',
+            loveIts: 1,
+            created_at: new Date()
+        },
+        {
+            title: 'Mon deuxieme Post',
+            content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce venenatis vulputate venenatis. Vivamus a sagittis arcu.',
+            loveIts: 0,
+            created_at: new Date()
+        },
+        {
+            title: 'Encore un post',
+            content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce venenatis vulputate venenatis. Vivamus a sagittis arcu.',
+            loveIts: -1,
+            created_at: new Date()
+        }
+    ];
 }
